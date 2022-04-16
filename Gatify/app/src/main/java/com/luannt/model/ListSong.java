71c881bd6ccfa7b4d6109b961fbfe501ea9f00a1
luanntps <99125936+luanntps@users.noreply.github.com>
2022-04-16 15:24:10 +0700

    1

--- Gatify/app/src/main/java/com/luannt/model/ListSong.java
+++
@@ -1,7 +0,0 @@
-package com.luannt.model;
-
-import java.util.ArrayList;
-
-public class ListSong {
-    private ArrayList<Song> alSong;
-}
--- Gatify/app/src/main/java/com/luannt/model/ListUser.java
+++
@@ -1,7 +0,0 @@
-package com.luannt.model;
-
-import java.util.ArrayList;
-
-public class ListUser {
-    private ArrayList<User> alUser;
-}
